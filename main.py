7cbb406e935ccfb3704b8309960a430cfeebb9f1
Fernando Guzman <fernando.guzman1@upr.edu>
2020-03-11 01:55:57 -0400

    added user routes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.resources import resources
+from routes.users import users
 
 app = Flask(__name__)
 app.register_blueprint(resources, url_prefix='/resource')
+app.register_blueprint(users, url_prefix='/user')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Welcome to our DB project'
---
+++ routes/users.py
@@ -0,0 +1,35 @@
+from flask import Blueprint
+from flask import request
+
+users = Blueprint('users', __name__)
+
+
+@users.route('/', methods = ['GET'])
+def user():
+    #GET handler code
+    return 'user GET'
+
+
+@users.route('/<int:id>', methods = ['GET', 'PUT', 'DELETE'])
+def userById(id):
+    if request.method == 'GET':
+        #GET handler code
+        return f'user/{id} GET'
+    elif request.method == 'PUT':
+        #PUT handler code
+        return f'user/{id} PUT'
+    elif request.method == 'DELETE':
+        #DELETE handler code
+        return f'user/{id} DELETE'
+
+
+@users.route('/log_in', methods = ['GET'])
+def userLogin():
+    #GET handler code
+    return 'user/log_in GET'
+
+
+@users.route('/sign_up/<int:role>', methods = ['POST'])
+def userSignup(role):
+    #POST handler code
+    return f'user/sign_up/{role} POST'
