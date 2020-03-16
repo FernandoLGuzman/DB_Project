78be09f296ec3411bc2e670ec3c8bd779d35bc6e
Jaime G. Torrens Lugo <jaime.torrens@upr.edu>
2020-03-15 20:50:30 -0400

    Added cats,  roles routes classes plus main calls

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from routes.resources import resources
+from routes.categories import categories
+from routes.roles import roles
 
 app = Flask(__name__)
 app.register_blueprint(resources, url_prefix='/resource')
+app.register_blueprint(categories, url_prefix='/category')
+app.register_blueprint(roles, url_prefix='/role')
 
 @app.route('/')
 def hello_world():
