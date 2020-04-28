607751e8962c447990c96c61a4778b4c298994f0
eduar <eduardo.oneill@upr.edu>
2020-04-28 19:42:05 -0400

    commented example

--- dao/resource.py
+++ dao/resource.py
@@ -2,4 +2,4 @@ import mysql.connector
 from mysql.connector import Error
 from config.localConfig import mysql as config
 
-connection = mysql.connector.connect(**config)
+# connection = mysql.connector.connect(**config)
