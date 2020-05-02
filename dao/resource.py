049bde0399fe2934f263ac8006f3accddbbf8184
eduar <eduardo.oneill@upr.edu>
2020-05-02 09:28:51 -0400

    get all resources dao

--- dao/resource.py
+++ dao/resource.py
@@ -3,3 +3,21 @@ from mysql.connector import Error
 from config.localConfig import mysql as config
 
 # connection = mysql.connector.connect(**config)
+
+class ResourceDao:
+
+    def __init__(self):
+        super().__init__()
+
+        self.connection = mysql.connector.connect(**config)
+
+    def getAllResources(self, limit = 25, offset = 0, orderBy = 'name'):
+        cursor = self.connection.cursor()
+        query = ("select * from resources "
+                "order by %s"
+                "limit %s offset %s")
+        
+        cursor.execute(query, (orderBy, limit, offset))
+        print(cursor.fetchone())
+
+# ResourceDao().getAllResources()
