b50643fe3dcf46c73df23cd6c1d43ff1e2439d56
Jaime G. Torrens Lugo <jaime.torrens@upr.edu>
2020-05-04 23:59:24 -0400

    Added request route to main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes.resources import resources
 from routes.users import users
 from routes.categories import categories
 from routes.roles import roles
+from routes.requests import requests
 
 from flask_cors import CORS, cross_origin
 
@@ -14,6 +15,7 @@ app.register_blueprint(resources, url_prefix='/resource')
 app.register_blueprint(users, url_prefix='/user')
 app.register_blueprint(categories, url_prefix='/category')
 app.register_blueprint(roles, url_prefix='/role')
+app.register_blueprint(requests, url_prefix='/request')
 
 @app.route('/')
 def hello_world():
