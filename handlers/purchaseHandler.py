35502249fa32bf4671d0637e2f09c8b5a72f1f73
Jaime G. Torrens Lugo <jaime.torrens@upr.edu>
2020-05-05 02:39:08 -0400

    Added purchaseHandler and route

---
+++ handlers/purchaseHandler.py
@@ -0,0 +1,73 @@
+from flask import jsonify
+from dao.purchase import PurchaseDao
+
+class PurchaseHandler:
+    def buildPurchase(self, row):
+        purchase = {
+        'purchase_id': row[0],
+        'user_id': row[1],
+        'resource_id': row[2],
+        'quantity': row[3],
+        'price': float(row[12]),
+        'date': row[5],
+        'address_id': row[8],
+        'category_id': row[9],
+        'resource_name': row[10],
+        'description': row[11],
+        'price': float(row[12]),
+        'stock': row[13]
+        }
+        return purchase
+
+    def getPurchases(self, args):
+        userId = args.get('userId', None)
+        resourceId = args.get('resourceId', None)
+        resourceName = args.get('resourceName', None)
+        date = args.get('date', None)
+        price = args.get('price', None)
+        limit = args.get('limit', 25)
+        offset = args.get('offset', 0)
+        orderBy = args.get('orderBy', 'PID')
+
+        purchaseList = []
+        if userId and not resourceId and not resourceName and not date and not price:
+            purchaseList = PurchaseDao().getPurchasesByUserID(userId, limit, offset, orderBy)
+        elif not userId and resourceId and not resourceName and not date and not price:
+            purchaseList = PurchaseDao().getPurchasesByResourceID(resourceId, limit, offset, orderBy)
+        elif userId and resourceId and not resourceName and not date and not price:
+            purchaseList = PurchaseDao().getPurchasesByUserIDAndResourceID(userId, resourceId, limit, offset, orderBy)
+        elif not userId and not resourceId and resourceName and not date and not price:
+            purchaseList = PurchaseDao().getPurchasesByResourceName(resourceName, limit, offset, orderBy)
+        elif not userId and not resourceId and not resourceName and date and not price:
+            purchaseList = PurchaseDao().getPurchasesByDate(date, limit, offset, orderBy)
+        elif not userId and not resourceId and not resourceName and not date and price:
+            purchaseList = PurchaseDao().getPurchasesByPrice(price, limit, offset, orderBy)
+        elif not userId and not resourceId and not resourceName and not date and not price:
+            purchaseList = PurchaseDao().getAllPurchases(limit, offset, orderBy)
+        else:
+            return jsonify(Error = "Malformed Get Request"), 400
+        
+        result = []
+        for row in purchaseList:
+            result.append(self.buildPurchase(row))
+        
+        return jsonify(Purchases = result), 200
+
+    def getPurchaseById(self, pid):
+        purchase = PurchaseDao().getPurchaseByID(pid)
+        if not purchase:
+            return jsonify(Error = "Purchase Not Found"), 404
+        result = self.buildPurchase(purchase)
+        return result
+    
+    def insertPurchase(self, args):
+        # TODO later
+        return
+
+    def updatePurchase(self, pid, args):
+        # TODO later
+        return
+
+    def deletePurchase(self, pid):
+        # TODO later
+        return
