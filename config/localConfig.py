287e3be74ef7ab95665e963db3784c9418db4cf2
eduar <eduardo.oneill@upr.edu>
2020-05-15 13:08:10 -0400

    modified config for remote db

--- config/localConfig.py
+++ config/localConfig.py
@@ -1,6 +1,22 @@
-mysql = {
-    "host": "localhost",
-    "username": "project-user",
-    "password": "CIIC-4060",
-    "database": "disaster_project"
-}
+import os
+
+mysql = {}
+
+try:
+    host = os.environ['host']
+    username = os.environ['username']
+    password = os.environ['password']
+    database = os.environ['database']
+
+    mysql['host'] = host
+    mysql['username'] = username
+    mysql['password'] = password
+    mysql['database'] = database
+    
+except:
+    mysql = {
+        "host": "localhost",
+        "username": "project-user",
+        "password": "CIIC-4060",
+        "database": "disaster_project"
+    }
